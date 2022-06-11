cc91df1531c524bb90d2e97f7d34358f8d45de4a
alvaropms <alvaromelquiades2001@gmail.com>
2022-06-11 16:11:22 -0300

    Prestadores por Cidade e Serviço funcionando

--- lib/util/getQtdePrestadoresDeServicoPorTipoSeervicoECidade.dart
+++ lib/util/getQtdePrestadoresDeServicoPorTipoSeervicoECidade.dart
@@ -1,20 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class GetQtdePrestadoresDeServicoPorTipoSeervicoECidade {
-  final String idCidade = '';
-  final int idServico = 0;
+  final String idCidade = 'Colatina - ES';
+  final int idServico = 2;
   final FirebaseFirestore _instance = FirebaseFirestore.instance;
 
   Future<int> action() async {
-    print('----------------uu--------------');
-
-    QuerySnapshot resp = await _instance
+    QuerySnapshot query = await _instance
         .collection('dadosPrestador')
-        .where('city', arrayContainsAny: [idCidade]).where('roles',
-            arrayContains: [idServico]).get();
+        .where('city', arrayContainsAny: [idCidade]).get();
+
+    List docs = query.docs;
+    docs.removeWhere((element) {
+      return !(element['roles'] as List).contains(idServico);
+    });
+
     print('------------------------------');
-    print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
-    print(resp.docs.length);
-    return resp.docs.length;
+    print('Quantidade de prestadores em ' +
+        idCidade +
+        ' que prestam o serviço ' +
+        idServico.toString() +
+        ': ' +
+        docs.length.toString());
+
+    return docs.length;
   }
 }
